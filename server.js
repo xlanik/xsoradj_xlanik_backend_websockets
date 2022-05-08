97014e7e4e5e88e45c534d5d02bee46304492554
Serginko <jakubsorad@gmail.com>
2022-05-08 16:10:10 +0200

    s

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const app = express();
 const port = process.env.PORT || 3000;
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
@@ -8,15 +7,21 @@ const multer  = require('multer')
 const upload = multer({ dest: 'uploads/' })
 const fs = require('fs');
 const cors = require('cors')
-
-
+const app = express()
+.use(cors)
+.use(express.json())
+.listen(process.env.PORT || port, () => console.log(`Listening on ${PORT}`))
 //const websocket = require('ws');
 
 const databaseUri = process.env.MONGODB_URI
 
 //app.use(express.json()) //https://stackoverflow.com/questions/18542329/typeerror-cannot-read-property-id-of-undefined
-app.use(express.json({limit: '50mb'}));
-app.use(cors())
+//app.use(express.json({limit: '50mb'}));
+//app.use(cors())
+
+
+
+
 //v envcku v tom database uri si treb premenit nazov databazy z MyfirstDatabase na Autoservis
 
 /*const soket = new websocket.Server({ 
@@ -347,10 +352,6 @@ app.get('/', function(req, res) {
   res.send('LanSor Autoservis zakladny endpoint funguje!')
 });
 
-
-app.listen(process.env.PORT || port, function() {
-  console.log(`App listening on port ${port}!`)
-});
 
 //middleware funkcia pouzita z linku. Ziskame si vzdy konkretny objekt alebo objekty
 //https://github.com/WebDevSimplified/Your-First-Node-REST-API/blob/master/routes/subscribers.js
